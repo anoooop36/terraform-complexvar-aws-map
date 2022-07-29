26a627f00a61fb67d946c9e672ff508e6668d40c
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-07-29 17:22:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
 
+provider "aws" {
+ access_key = var.access_key
+ secret_key = var.secret_key
+ region     = var.region
+}
+
+ resource "aws_ebs_volume" "awsEbsExampleWithMap" {
+  availability_zone = "us-east-1a"
+  size = var.sampleMap["size"]
+  tags = {
+    Name = var.sampleMap["tag"]
+  }
+}
