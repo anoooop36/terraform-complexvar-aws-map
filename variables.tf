e75b09fdfef39dab6491311043c1c1f59e2cdc10
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-08-02 16:30:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
+variable "access_key" {}
+variable "secret_key" {}
 
+variable "region" {
+ type    = string
+ default = "us-east-1"
+}
+
+
+variable "sampleMap" {
+  type = map(string)
+  default = {
+    size : "40"
+    tag = "Dev ebs volume", 
+  }
+}
