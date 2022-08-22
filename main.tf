8392c6b4d8c0e54e19119a1f07c5ecabe0ea9843
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-08-22 13:11:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
-/*
 provider "aws" {
  access_key = var.access_key
  secret_key = var.secret_key
  region     = var.region
 } 
-*/
 
  resource "aws_ebs_volume" "awsEbsExampleWithMap" {
   availability_zone = "us-east-1a"
